3b7a5ead857b807a3d2935ef67cb224be00c3abe
tom mobile <prosto@svejee.ru>
2021-08-09 22:40:28 +0300

    Apple base model

--- appleapp/models/Apple.php
+++ appleapp/models/Apple.php
@@ -16,12 +16,12 @@ class Apple
     public $color;
 
     /**
-     * @var \DateTime Дата появления
+     * @var integer Дата появления
      */
     private $_created;
 
     /**
-     * @var \DateTime Дата падения
+     * @var integer Дата падения
      */
     private $_fallen;
 
@@ -37,7 +37,7 @@ class Apple
 
     /**
      * Apple constructor.
-     * @param $color
+     * @param string $color
      * @throws \Exception
      */
     public function __construct($color)
@@ -46,6 +46,137 @@ class Apple
         $this->_state = self::STATE_ON_TREE;
         $this->_health = 100;
         $this->_created = $this->_getCreatedTime();
+    }
+
+    /**
+     * @return int
+     */
+    public function getState()
+    {
+        return $this->_state;
+    }
+
+    /**
+     * @return integer
+     */
+    public function getCreatedDate()
+    {
+        return $this->_created;
+    }
+
+    /**
+     * @return integer
+     */
+    public function getFallenDate()
+    {
+        return $this->_fallen;
+    }
+
+    /**
+     * @return float
+     */
+    public function getSize()
+    {
+        return $this->_health / 100;
+    }
+
+    /**
+     * Падение яблока
+     *
+     * @return bool
+     * @throws \Exception
+     */
+    public function fall()
+    {
+        if(self::STATE_ON_TREE === $this->_state) {
+            $this->_state = self::STATE_ON_GROUND;
+            $this->_fallen = time();
+
+            return true;
+        }
+
+        throw new AppleException('Это яблоко не может упасть');
+    }
+
+    /**
+     * Откусить от яблока
+     *
+     * @param int $size
+     * @return bool
+     */
+    public function eat($size = 100)
+    {
+        if(self::STATE_ON_GROUND !== $this->_state) {
+            throw new AppleException('Яблоко не лежит на земле');
+        }
+
+        if($size <= 0 || $size > $this->_health) {
+            throw new AppleException("Нельзя столько откусить");
+        }
+
+        $this->_health -= $size;
+        if(0 === $this->_health) {
+            $this->delete();
+        }
+
+        return true;
+    }
+
+    /**
+     * удалить яблоко
+     *
+     * @return bool
+     */
+    public function delete()
+    {
+        if(0 !== $this->_health) {
+            throw new AppleException('Нельзя удалить недоеденное яблоко');
+        }
+
+        $this->_state = self::STATE_EATEN;
+
+        return true;
+    }
+
+    /**
+     * испортить яблоко
+     *
+     * @return bool
+     */
+    public function spoil()
+    {
+        if(self::STATE_ON_GROUND === $this->_state) {
+            $this->_state = self::STATE_ROTTEN;
+            return true;
+        }
+
+        throw new AppleException("Это яблоко не может испортиться");
+    }
+
+    /**
+     * getters magic
+     *
+     * @param $name
+     * @return mixed
+     * @throws AppleException
+     */
+    public function __get($name)
+    {
+        $getter = 'get' . $name;
+        if (method_exists($this, $getter)) {
+            return $this->$getter();
+        }
+
+        throw new AppleException('Getting unknown property: ' . get_class($this) . '::' . $name);
+    }
+
+    /**
+     * @return int
+     */
+    protected function _getCreatedTime()
+    {
+        $randomTime = (time() - mt_rand(0, 60*60*24*7)); // случайно в пределах недели
+        return $randomTime;
     }
 
 }
